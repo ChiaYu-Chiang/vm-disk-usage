22312e3bf31c6702a769843f3d7d540dad073c83
Brian Chiang <brian_chiang@chief.com.tw>
2024-07-22 10:24:29 +0800

    adjust email content & add except type & add nmap scan function

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import json
 import time
-from ssh_utils import SSHClient
+from ssh_utils import SSHClient, scan_network
 from parser_utils import parse_linux_output, parse_windows_output
 from sendmail import send_email
+import socket, paramiko
 
 
 if __name__ == "__main__":
     starttime = time.time()
 
     target_percent = 80
+    default_target_info = {
+        "name": "S3_HAPROXY",
+        "port": 22,
+        "user": "root",
+        "pwd": "Chief+26576688@",
+        "os": "linux"
+    }
     scanned_vms = []
+    scanned_failed_vms = []
     high_usage_ips = []
+    targets = []
+    
+    network = "10.210.68.0/24"
+    live_hosts = scan_network(network)
+
+    for ip in live_hosts:
+        target_info = default_target_info.copy()
+        target_info["ip"] = ip
+        targets.append(target_info)
+    print(f"targets after ip scan: \n{targets}")
 
     with open("targets.json") as json_file:
-        targets = json.load(json_file)
+        json_targets = json.load(json_file)
+        targets.extend(json_targets)
 
     ssh = SSHClient()
 
@@ -26,45 +46,60 @@ if __name__ == "__main__":
     }
 
     for target in targets:
+        if target["os"] == "windows":
+            scanned_failed_vms.append((target["ip"], target["name"]))
+            print("skip this windows target.")
+            continue
         try:
             ssh.connect(target["ip"], target["port"], target["user"], target["pwd"])
-        except Exception as e:
+            command, parser = command_and_parser.get(target["os"], (None, None))
+            if command and parser:
+                output = ssh.execute_command(
+                    command, "utf-8" if target["os"] == "linux" else "cp950"
+                )
+                if output is not None:
+                    usages = parser(output)
+                    for usage in usages:
+                        if usage >= target_percent and target["ip"] not in high_usage_ips:
+                            high_usage_ips.append(target["ip"])
+                        scanned_vms.append((target["ip"], target["name"]))
+                    print(f"ip: {target['ip']}, usage: {usages}%")
+        except (paramiko.AuthenticationException, paramiko.SSHException, socket.timeout, Exception) as e:
+            scanned_failed_vms.append((target["ip"], target["name"]))
             print(f"Error connecting to {target['ip']}: {e}")
             continue
-        command, parser = command_and_parser.get(target["os"], (None, None))
-        if command and parser:
-            output = ssh.execute_command(
-                command, "utf-8" if target["os"] == "linux" else "cp950"
-            )
-            if output is not None:
-                usages = parser(output)
-                for usage in usages:
-                    if usage >= target_percent and target["ip"] not in high_usage_ips:
-                        high_usage_ips.append(target["ip"])
-                    scanned_vms.append((target["ip"], target["name"]))
-                print(f"ip: {target['ip']}, usage: {usages}%")
+        finally:
+            ssh.close()
     
     if high_usage_ips:
         email_body = f"<span style='color: red;'>檢測到高於磁碟使用率 {target_percent}% 的VM IP:<br>"
         for ip in high_usage_ips:
             print(ip)
             email_body += ip + "<br>"
+        email_body += "</span>"
     else:
-        email_body = f"<span style='color: green;'>未檢測到高於磁碟使用率 {target_percent}% 的VM<br>"
+        email_body = f"<span style='color: green;'>未檢測到高於磁碟使用率 {target_percent}% 的VM<br></span>"
     print(email_body)
 
     if scanned_vms:
-        email_body += f"</span>已掃描VM:<br>"
+        email_body += f"已掃描VM:<br>"
         for vm in scanned_vms:
             print(vm)
             print(vm[0])
             email_body += f"{vm[0]} {vm[1]}<br>"
 
+    if scanned_failed_vms:
+        email_body += f"<span style='color: red;'>未掃描VM:<br>"
+        for vm in scanned_failed_vms:
+            print(vm)
+            print(vm[0])
+            email_body += f"{vm[0]} {vm[1]}<br>"
+        email_body += "</span>"
+
     subject = "高磁碟使用率通知"
+    brian_email = "brian_chiang@chief.com.tw"
     to_email = "brian_chiang@chief.com.tw, marco_li@chief.com.tw, aaron_lin@chief.com.tw, allen_yang@chief.com.tw"
     send_email(subject, email_body, to_email)
-
-    ssh.close()
 
     endtime = time.time()
     deltatime = endtime - starttime
--- ssh_utils.py
+++ ssh_utils.py
@@ -1,4 +1,6 @@
 import paramiko
+import nmap
+import socket
 
 
 class SSHClient:
@@ -10,17 +12,59 @@ class SSHClient:
         try:
             self.ssh.connect(ip, username=user, port=port, password=pwd)
             print("Connected successfully.")
+        except paramiko.AuthenticationException:
+            print(f"Error: Authentication failed for {ip}")
+            raise
+        except paramiko.SSHException as e:
+            print(f"Error: SSH error for {ip}: {e}")
+            raise
+        except socket.timeout:
+            print(f"Error: Connection timed out for {ip}")
+            raise
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error: Unexpected error for {ip}: {e}")
+            raise
 
     def execute_command(self, command, encoding):
         try:
             _, stdout, _ = self.ssh.exec_command(command)
-            output = stdout.read().decode(encoding)
-            return output
+            if stdout:
+                output = stdout.read().decode(encoding)
+                return output
+            else:
+                print("Error: Command execution failed.")
+                return None
         except Exception as e:
             print(f"Error: {e}")
+            return None
 
     def close(self):
         self.ssh.close()
         print("SSH connection closed.")
+
+
+def scan_network(network):
+    nm = nmap.PortScanner()
+    print(f"Scanning network: {network}")
+    nm.scan(hosts=network, arguments='-p 22 --open')
+    live_hosts = nm.all_hosts()
+    print(f"Scan result: {live_hosts}")
+    return live_hosts
+
+if __name__ == "__main__":
+    network = '10.210.68.0/24'
+    live_hosts = scan_network(network)
+    default_target_info = {
+        "name": "S3_HAPROXY",
+        "port": 22,
+        "user": "root",
+        "pwd": "Chief+26576688@",
+        "os": "linux"
+    }
+    targets = []
+    for ip in live_hosts:
+        target_info = default_target_info.copy()
+        target_info["name"] = target_info["name"] + "_" + ip
+        target_info["ip"] = ip
+        targets.append(target_info)
+    print(f"targets after ip scan: \n{targets}")
