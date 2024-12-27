a89c4527b9c8e18acabdfd250cb11b3a2dd7f1a7
Brian Chiang <brian_chiang@chief.com.tw>
2024-12-27 17:30:19 +0800

    add mail message

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import socket, paramiko
 if __name__ == "__main__":
     starttime = time.time()
 
+    # 設定磁碟使用率警告閾值
     target_percent = 80
     default_target_info = {
         "name": "S3_HAPROXY",
@@ -25,18 +26,21 @@ if __name__ == "__main__":
     network = "10.210.68.0/24"
     live_hosts = scan_network(network)
 
+    # 將掃描到的活躍主機添加到列表
     for ip in live_hosts:
         target_info = default_target_info.copy()
         target_info["ip"] = ip
         targets.append(target_info)
     print(f"targets after ip scan: \n{targets}")
 
+    # 從JSON文件中讀取額外的目標主機
     with open("targets.json") as json_file:
         json_targets = json.load(json_file)
         targets.extend(json_targets)
 
     ssh = SSHClient()
 
+    # 定義不同操作系統的檢查命令和解析函數
     command_and_parser = {
         "linux": ("df /", parse_linux_output),
         "windows": (
@@ -51,6 +55,7 @@ if __name__ == "__main__":
             print("skip this windows target.")
             continue
         try:
+            # 嘗試SSH連接到目標主機
             ssh.connect(target["ip"], target["port"], target["user"], target["pwd"])
             command, parser = command_and_parser.get(target["os"], (None, None))
             if command and parser:
@@ -71,16 +76,19 @@ if __name__ == "__main__":
         finally:
             ssh.close()
     
+    # 生成郵件內容
+    email_body = f"<span>總數: {len(targets)} 已掃描: {len(scanned_vms)} 未掃描: {len(scanned_failed_vms)}<br></span>"
     if high_usage_ips:
-        email_body = f"<span style='color: red;'>檢測到高於磁碟使用率 {target_percent}% 的VM IP:<br>"
+        email_body += f"<span style='color: red;'>檢測到高於磁碟使用率 {target_percent}% 的VM IP:<br>"
         for ip in high_usage_ips:
             print(ip)
             email_body += ip + "<br>"
         email_body += "</span>"
     else:
-        email_body = f"<span style='color: green;'>未檢測到高於磁碟使用率 {target_percent}% 的VM<br></span>"
+        email_body += f"<span style='color: green;'>未檢測到高於磁碟使用率 {target_percent}% 的VM<br></span>"
     print(email_body)
 
+    # 添加已掃描VM的信息
     if scanned_vms:
         email_body += f"已掃描VM:<br>"
         for vm in scanned_vms:
@@ -88,6 +96,7 @@ if __name__ == "__main__":
             print(vm[0])
             email_body += f"{vm[0]} {vm[1]}<br>"
 
+    # 添加掃描失敗VM的信息
     if scanned_failed_vms:
         email_body += f"<span style='color: red;'>未掃描VM:<br>"
         for vm in scanned_failed_vms:
@@ -96,6 +105,7 @@ if __name__ == "__main__":
             email_body += f"{vm[0]} {vm[1]}<br>"
         email_body += "</span>"
 
+    # 發送郵件通知
     subject = "高磁碟使用率通知"
     brian_email = "brian_chiang@chief.com.tw"
     to_email = "brian_chiang@chief.com.tw, marco_li@chief.com.tw, aaron_lin@chief.com.tw, allen_yang@chief.com.tw"
